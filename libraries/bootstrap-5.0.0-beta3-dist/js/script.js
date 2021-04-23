322785c94f3f715318658aeca6fcb44b97a8591a
faehsani2 <fhsani2020@gmail.com>
2021-04-23 22:44:43 +0430

    commited added new files

---
+++ libraries/bootstrap-5.0.0-beta3-dist/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  $("#slider").owlCarousel({
+    loop: true,
+    dots: true,
+    rtl: true,
+    items: 1,
+    animateIn: "fadeIn",
+    animateOut: "fadeOut",
+    margin: 0,
+  });
+});

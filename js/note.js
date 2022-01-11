7031353989f39a0f03363d87ece5b3283f59457a
javad feyzi <jfeyzi69@gmail.com>
2022-01-11 15:56:51 +0330

    add testimony section

---
+++ js/note.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
